8f858ea7243c36f771e26eb4f2a62d65479f9bd4
Danilova09 <93181066+Danilova09@users.noreply.github.com>
2021-10-28 16:00:05 +0600

    Реализация функций для случайного набора чисел от 5 до 36

--- my-apps/src/app/numbers-block/numbers-block.component.ts
+++ my-apps/src/app/numbers-block/numbers-block.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-numbers-block',
   templateUrl: './numbers-block.component.html',
   styleUrls: ['./numbers-block.component.css']
 })
-export class NumbersBlockComponent implements OnInit {
+export class NumbersBlockComponent {
+  arrayNumbers: number[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.arrayNumbers = this.generateNewArrayNumbers();
   }
 
+  getRandomIntNumber(min: number = 5, max: number = 36) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  generateNewArrayNumbers() {
+    const array: number[] = [];
+    while (array.length < 5) {
+      let randomNumber = this.getRandomIntNumber();
+      if (!array.includes(randomNumber)) {
+        array.push(randomNumber);
+      }
+    }
+    array.sort((a, b) => {
+      return a - b
+    });
+    this.arrayNumbers = array;
+    return array;
+  }
 }
