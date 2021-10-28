e63ee6db807fe53c4ac5fe3bc9d1ab5676947773
Danilova09 <93181066+Danilova09@users.noreply.github.com>
2021-10-28 16:24:20 +0600

    Реализация компонента card. Добавление основных стилей

--- my-apps/src/app/app.module.ts
+++ my-apps/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { NumbersBlockComponent } from './numbers-block/numbers-block.component';
 import { NumbersItemComponent } from './numbers-item/numbers-item.component';
 import { MainGalleryBlockComponent } from './main-gallery-block/main-gallery-block.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NumbersBlockComponent,
     NumbersItemComponent,
-    MainGalleryBlockComponent
+    MainGalleryBlockComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
