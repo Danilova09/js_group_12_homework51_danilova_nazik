5a7b889f4974de47867fb0c6188185991ec54a69
Danilova09 <93181066+Danilova09@users.noreply.github.com>
2021-10-28 15:54:33 +0600

    Создание компонента numbers-block. Добавление основных стилоей

---
+++ my-apps/src/app/numbers-block/numbers-block.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-numbers-block',
+  templateUrl: './numbers-block.component.html',
+  styleUrls: ['./numbers-block.component.css']
+})
+export class NumbersBlockComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
